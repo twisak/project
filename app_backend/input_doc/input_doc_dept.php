5fb4ab713f0c08cdc95f9d7ef62bb9216fea006a
594407088 <594407088@parichat.skru.ac.th>
2020-06-04 00:12:45 +0700

    input_doc

--- app_backend/input_doc/input_doc_dept.php
+++ app_backend/input_doc/input_doc_dept.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
     <title>AdminWrap - Easy to Customize Bootstrap 4 Admin Template</title>
     <!-- Bootstrap Core CSS -->
     <link href="../assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -145,10 +145,10 @@
                 <!-- ============================================================== -->
                 <div class="row page-titles">
                     <div class="col-md-5 align-self-center">
-                        <h3 class="text-themecolor">ตารางเอกสารสัญญายืม</h3>
+                        <h3 class="text-themecolor">เอกสารล้างหนี้</h3>
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                            <li class="breadcrumb-item active">Table Basic</li>
+                            <li class="breadcrumb-item active">เอกสารล้างหนี้</li>
                         </ol>
                     </div>
                     <div class="col-md-7 align-self-center">
@@ -161,54 +161,170 @@
                 <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
+                <!-- Row -->
                 <div class="row">
-                    <!-- column -->
-                    <div class="col-12">
+                    <!-- Column -->
 
+                    <!-- <div class="col-lg-4 col-xlg-3 col-md-5">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title">เอกสารสัญญายืม</h4>
-                                <!-- <h6 class="card-subtitle">Add class <code>.table</code></h6> -->
+                                <center class="m-t-30"> <img src="../assets/images/users/5.jpg" class="img-circle" width="150" />
+                                    <h4 class="card-title m-t-10">Hanna Gover</h4>
+                                    <h6 class="card-subtitle">Accoubts Manager Amix corp</h6>
+                                    <div class="row text-center justify-content-md-center">
+                                        <div class="col-4"><a href="javascript:void(0)" class="link"><i class="icon-people"></i> <font class="font-medium">254</font></a></div>
+                                        <div class="col-4"><a href="javascript:void(0)" class="link"><i class="icon-picture"></i> <font class="font-medium">54</font></a></div>
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div> -->
+
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-12 col-xlg-9 col-md-7">
+                        <div class="card">
+                            <!-- Tab panes -->
+                            <div class="card-body">
+                                <form class="form-horizontal form-material">
+
+<div class="row">
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+    </div>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+    </div>
+</div>
+
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+    </div>
+</div>
+
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+    </div>
+</div>
+
+<div class="col-md-3">
+<div class="form-group">
+<label>Full Name</label>
+<input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+    </div>
+</div>
+</div>
+   
+
+   
+<div class="row">
+<div class="col-md-3">  
+</div>
+<div class="col-md-3">  
+<div class="form-group">
+<button type="button" class="btn btn-primary btn-block">บันทึก</button>
+    </div>
+</div>
+
+<div class="col-md-3">  
+<div class="form-group">
+<button type="button" class="btn btn-danger btn-block">ยกเลิก</button>
+    </div>
+</div>
+
+</div>
+ 
 <!-- 
-                                <div class="container">
-                                <div class="row">
-                                    <div class="col-md-1 offset-md-10">ssss</div>
-                                </div>
-                                </div> -->
-
-
-                                <div class="text-right"><button type="button" class="btn btn-primary">กรอกเอกสารเพิ่ม</button></div>
-                                <div class="table-responsive">
-                                    <table class="table text-center">
-                                        <thead>
-                                        <tr>
-                                                <th class="text-center">#</th>
-                                                <th class="text-center">First Name</th>
-                                                <th class="text-center">Last Name</th>
-                                                <th class="text-center">Username</th>
-                                                <th class="text-center">แก้ไข</th>
-                                                <th class="text-center">ลบ</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Deshmukh</td>
-                                                <td>Prohaska</td>
-                                                <td>@Genelia</td>
-                                                <td><button type="button" class="btn btn-warning">แก้ไข</button></td>
-                                                <td><button type="button" class="btn btn-danger">ลบ</button></td>
-                                            </tr>
-
-
-
-                                        </tbody>
-                                    </table>
-                                </div>
+                                    <div class="form-group">
+                                        <label class="col-md-2">Full Name</label>
+                                        <div class="col-md-2">
+                                            <input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+                                        </div>
+                                        
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="example-email" class="col-md-2">Email</label>
+                                        <div class="col-md-2">
+                                            <input type="email" placeholder="johnathan@admin.com" class="form-control form-control-line" name="example-email" id="example-email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Password</label>
+                                        <div class="col-md-12">
+                                            <input type="password" value="password" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Phone No</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="123 456 7890" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Message</label>
+                                        <div class="col-md-12">
+                                            <textarea rows="5" class="form-control form-control-line"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-12">Select Country</label>
+                                        <div class="col-sm-12">
+                                            <select class="form-control form-control-line">
+                                                <option>London</option>
+                                                <option>India</option>
+                                                <option>Usa</option>
+                                                <option>Canada</option>
+                                                <option>Thailand</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-12">
+                                            <button class="btn btn-success">Update Profile</button>
+                                        </div>
+                                    </div> -->
+                                </form>
                             </div>
                         </div>
                     </div>
+                    <!-- Column -->
                 </div>
+                <!-- Row -->
                 <!-- ============================================================== -->
                 <!-- End PAge Content -->
                 <!-- ============================================================== -->
@@ -248,9 +364,6 @@
     <script src="../js/sidebarmenu.js"></script>
     <!--Custom JavaScript -->
     <script src="../js/custom.min.js"></script>
-    <!-- jQuery peity -->
-    <script src="../assets/node_modules/peity/jquery.peity.min.js"></script>
-    <script src="../assets/node_modules/peity/jquery.peity.init.js"></script>
 </body>
 
 </html>
