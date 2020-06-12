7f251154c3f3ff49d8fceb67b277b557bad577e6
594407088 <594407088@parichat.skru.ac.th>
2020-06-13 01:24:33 +0700

    activity

--- app_backend/project/INSERT_project.php
+++ app_backend/project/INSERT_project.php
@@ -4,43 +4,23 @@
     error_reporting(~0);
     include '../../administrator/connect.php';
 
-        $project_id = $_POST['project_id'];
-        $project_name = $_POST['project_name'];
-        // $hdn = $_POST['hdnCount'];
-        // echo $activity;
+		$project_id = $_POST['project_id'];
+		$project_name = $_POST['project_name'];
+		$fiscal_year = $_POST['fiscal_year'];
+		$budget_id = $_POST['budget_id'];
+		$product_id = $_POST['product_id'];
+		$mission_id = $_POST['mission_id'];
+		$strategic_id = $_POST['strategic_id'];
+		$principle = $_POST['principle'];
+		$budget = $_POST['budget'];
 
-	for ($i = 1; $i<= (int)$_POST["hdnCount"];$i++){
-		if(isset($_POST["activity$i"]))
-		{
-            $activity = $_POST["activity$i"];
-            //echo $activity;
-			if($activity != "")
-			{
-				$sql = "INSERT INTO tb_activity (project_id,activity)
-				 	     VALUES ('$project_id','$activity')";
-                $db_query = mysqli_query($conn,$sql);
-                //echo $sql;
-                //echo $db_query;
-                //echo "<br>";
-			}
-		}
-    }
-    //echo "<br>";
-    $sql1 = "INSERT INTO tb_project (project_id, project_name)
-			VALUES ('$project_id','$project_name')";
-            $db_query1 = mysqli_query($conn,$sql1);
-            //echo $sql1;
-            //echo $db_query1;
+		$sql_tb_project = "INSERT INTO tb_project(project_id,project_name,fiscal_year,budget_id,product_id,mission_id,strategic_id,principle,budget) VALUES ('".$project_id."','".$project_name."','".$fiscal_year."','".$budget_id."','".$product_id."','".$mission_id."','".$strategic_id."','".$principle."','".$budget."')";
 
-    mysqli_close($conn);
+		$query_tb_project = mysqli_query($conn,$sql_tb_project);
 
-    if($db_query)
-        {
-            echo "<script>";
-            echo "alert(\" บันทึกเรียบร้อย\");";
-            echo "</script>";
-            echo "<meta http-equiv='refresh'content='0;url=http://localhost/project_student/app_backend/project/tb_project.php'>";
-        }
+		echo "<script>alert('บันทึกข้อมูลของท่านเรียบร้อยแล้ว')</script>";
+		echo "<script>window.location='form_project.php'</script>";
+
 ?>
 </body>
 </html>
