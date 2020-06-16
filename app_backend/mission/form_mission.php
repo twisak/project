76aa4317408a0d1f4b80c23626677b130d22fe5d
594407088 <594407088@parichat.skru.ac.th>
2020-06-16 23:43:00 +0700

    detail

---
+++ app_backend/mission/form_mission.php
@@ -0,0 +1,216 @@
+<?php session_start();
+if($_SESSION['status'] == 'admin')
+{
+}
+elseif($_SESSION['status'] == 'staff')
+{
+}
+else
+{
+    echo "<script>";
+    echo "alert(\"คุณไม่มีสิทธิ์เข้าสู่ระบบ\");";
+    echo "</script>";
+    echo "<meta http-equiv='refresh' content='0;url=../../administrator/logout.php'>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>AdminWrap - Easy to Customize Bootstrap 4 Admin Template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="../css/style.css" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="../css/colors/default.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="fix-header card-no-border fix-sidebar">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="loader">
+            <div class="loader__figure"></div>
+            <p class="loader__label">Admin Wrap</p>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+        <?php include '../mamu/manu_admin.php'; ?>
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Bread crumb and right sidebar toggle -->
+                <!-- ============================================================== -->
+                <div class="row page-titles">
+                    <div class="col-md-5 align-self-center">
+                        <h3 class="text-themecolor">เพิ่มโครงการ</h3>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                            <li class="breadcrumb-item active">เพิ่มโครงการ</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-7 align-self-center">
+                        <a href="https://wrappixel.com/templates/adminwrap/" class="btn waves-effect waves-light btn btn-info pull-right hidden-sm-down"> Upgrade to Pro</a>
+                    </div>
+                </div>
+
+                <script src="http://code.jquery.com/jquery-latest.js"></script>
+
+
+                <div class="row">
+
+                    <div class="col-lg-12 col-xlg-9 col-md-7">
+                        <div class="card">
+                            <!-- Tab panes -->
+                            <div class="card-body">
+                                <?php
+                                        include '../../administrator/connect.php';
+                                        $sql = "Select Max(substr(project_id,3)+1) as MaxID from tb_project ";
+                                        $query = mysqli_query($conn,$sql);
+                                        $table_id = mysqli_fetch_assoc($query);
+                                        $testid = $table_id['MaxID'];
+                                                if($testid=='')
+                                                {
+                                                    $id="P001";
+                                                }else
+                                                {
+                                                    $id="P".sprintf("%03d",$testid);
+                                                }
+
+                            ?>
+                                <form class="form-horizontal form-material" action="insert_project.php" name="form_user" method="post">
+
+
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label>รหัสพันธกิจ</label>
+                                                <input type="text" name="project_id" value="<?=$id?>" placeholder="" class="form-control form-control-line">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>พันธกิจ</label>
+                                                <input type="text" name="project_name" placeholder="" class="form-control form-control-line">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary btn-block">บันทึก</button>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <button type="button" class="btn btn-danger btn-block">ยกเลิก</button>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                            </div>
+                        </div>
+                        <!-- Column -->
+                    </div>
+                    <!-- Row -->
+                    <!-- ============================================================== -->
+                    <!-- End PAge Content -->
+                    <!-- ============================================================== -->
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Container fluid  -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- footer -->
+                <!-- ============================================================== -->
+                <footer class="footer">
+                    © 2018 Adminwrap by wrappixel.com
+                </footer>
+                <!-- ============================================================== -->
+                <!-- End footer -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Page wrapper  -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- All Jquery -->
+        <!-- ============================================================== -->
+        <script src="../assets/node_modules/jquery/jquery.min.js"></script>
+        <!-- Bootstrap tether Core JavaScript -->
+        <script src="../assets/node_modules/bootstrap/js/popper.min.js"></script>
+        <script src="../assets/node_modules/bootstrap/js/bootstrap.min.js"></script>
+        <!-- slimscrollbar scrollbar JavaScript -->
+        <script src="../js/perfect-scrollbar.jquery.min.js"></script>
+        <!--Wave Effects -->
+        <script src="../js/waves.js"></script>
+        <!--Menu sidebar -->
+        <script src="../js/sidebarmenu.js"></script>
+        <!--Custom JavaScript -->
+        <script src="../js/custom.min.js"></script>
+
+</body>
+
+</html>
+<!-- <script type="text/javascript">
+$(document).ready(function(){
+
+    var rows = 1;
+    $("#createRows_activity").click(function(){
+
+
+        var tr = "<tr>";
+tr = tr + "<td><div class='row'><div class='col-md-4'><div class='form-group'><label>หัวข้อกิจกรรม</label><input type='text' class='form-control p_input'  name='activity"+rows+"'></div></div></td>";
+        tr = tr + "</tr>";
+        $('#myTable_activity > tbody:last').append(tr);
+
+        $('#hdnCount_activity').val(rows);
+        rows = rows + 1;
+    });
+
+    $("#deleteRows_activity").click(function(){
+        if ($("#myTable_activity tr").length != 1) {
+            $("#myTable_activity tr:last").remove();
+        }
+    });
+
+    $("#clearRows_activity").click(function(){
+        rows = 1;
+        $('#hdnCount_activity').val(rows);
+        $('#myTable_activity > tbody:last').empty(); // remove all
+    });
+
+});
+</script> -->
