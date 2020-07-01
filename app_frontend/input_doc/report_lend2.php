161fe53282154d8684f758d39190d615a1ac2e9d
twisak <594407058@parichat.skru.ac.th>
2020-07-02 03:14:35 +0700

    เอกสารสัญญายืม เหลือหน้า2

--- app_frontend/input_doc/report_lend2.php
+++ app_frontend/input_doc/report_lend2.php
@@ -1,21 +1,5 @@
-<!-- <?php session_start();
-if($_SESSION['status'] == 'admin')
-{
-}
-elseif($_SESSION['status'] == 'staff')
-{
-}
-else
-{
-    echo "<script>";
-    echo "alert(\"คุณไม่มีสิทธิ์เข้าสู่ระบบ\");";
-    echo "</script>";
-    echo "<meta http-equiv='refresh' content='0;url=../../administrator/logout.php'>";
-}
-?> -->
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -27,10 +11,10 @@ else
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
     <title>AdminWrap - Easy to Customize Bootstrap 4 Admin Template</title>
     <!-- Bootstrap Core CSS -->
-    <link href="../assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap_plugin.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="../css/style.css" rel="stylesheet">
     <link href="../css/report.css" rel="stylesheet">
+    <link href="../assets/node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- You can change the theme colors from here -->
     <link href="../css/colors/default.css" id="theme" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -39,129 +23,8 @@ else
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
-<head>
-    <meta charset="utf-8">
-<title>css print report table continue</title>
-<style type="text/css">
-* {
-    margin:0;
-    padding:0;
-    font-family:Arial, "times New Roman", tahoma;
-    font-size:12px;
-}
-html {
-    font-family:Arial, "times New Roman", tahoma;
-    font-size:12px;
-    color:#000000;
-}
-body {
-    font-family:Arial, "times New Roman", tahoma;
-    font-size:12px;
-    padding:0;
-    margin:0;
-    color:#000000;
-}
-.headTitle {
-    font-size:12px;
-    font-weight:bold;
-    text-transform:uppercase;
-}
-.headerTitle01 {
-    border:1px solid #333333;
-    border-left:2px solid #000;
-    border-bottom-width:2px;
-    border-top-width:2px;
-    font-size:11px;
-}
-.headerTitle01_r {
-    border:1px solid #333333;
-    border-left:2px solid #000;
-    border-right:2px solid #000;
-    border-bottom-width:2px;
-    border-top-width:2px;
-    font-size:11px;
-}
-/* สำหรับช่องกรอกข้อมูล  */
-.box_data1 {
-    font-family:Arial, "times New Roman", tahoma;
-    height:18px;
-    border:0px dotted #333333;
-    border-bottom-width:1px;
-}
-/* กำหนดเส้นบรรทัดซ้าย  และด้านล่าง */
-.left_bottom {
-    border-left:2px solid #000;
-    border-bottom:1px solid #000;
-}
-/* กำหนดเส้นบรรทัดซ้าย ขวา และด้านล่าง */
-.left_right_bottom {
-    border-left:2px solid #000;
-    border-bottom:1px solid #000;
-    border-right:2px solid #000;
-}
-/* สร้างช่องสี่เหลี่ยมสำหรับเช็คเลือก */
-.chk_box {
-    display:block;
-    width:10px;
-    height:10px;
-    overflow:hidden;
-    border:1px solid #000;
-}
-/* css ส่วนสำหรับการแบ่งหน้าข้อมูลสำหรับการพิมพ์ */
-@media all
-{
-    .page-break { display:none; }
-    .page-break-no{ display:none; }
-}
-@media print
-{
-    .page-break { display:block;height:1px; page-break-before:always; }
-    .page-break-no{ display:block;height:1px; page-break-after:avoid; }
-}
-</style>
 </head>
-</head>
-<?php
 
-     include '../../administrator/connect.php';
-     $doc_id =$_GET['id'];
-     $sql ="SELECT * FROM tb_contract WHERE doc_id = '".$doc_id."'";
-     $query = mysqli_query($conn,$sql);
-     while($row = mysqli_fetch_array($query,MYSQLI_ASSOC))
-     {
-         $doc_id = $row['doc_id'];
-         $foreword = $row['foreword'];
-         $str_date = $row['str_date'];
-         $stp_date = $row['stp_date'];
-         $project_id = $row['project_id'];
-         $person_id = $row['person_id'];
-     }
-
-     $sql1 ="SELECT * FROM tb_project WHERE project_id = '".$project_id."' ";
-     $query1 = mysqli_query($conn,$sql1);
-     while($row1 = mysqli_fetch_array($query1,MYSQLI_ASSOC))
-     {
-         $project_name = $row1['project_name'];
-         $project_id = $row1['project_id'];
-     }
-
-     $sql3 ="SELECT * FROM tb_activity WHERE project_id = '".$project_id."' ";
-     $query3 = mysqli_query($conn,$sql3);
-     while($row3 = mysqli_fetch_array($query3,MYSQLI_ASSOC))
-     {
-         $activity = $row3['activity'];
-     }
-
-     $sql2 ="SELECT * FROM tb_person WHERE person_id = '".$person_id."' ";
-     $query2 = mysqli_query($conn,$sql2);
-     while($row2 = mysqli_fetch_array($query2,MYSQLI_ASSOC))
-     {
-         $prefix = $row2['prefix'];
-         $firtname = $row2['firtname'];
-         $lastname = $row2['lastname'];
-         //$prefix = $row2['prefix'];
-     }
-?>
 <body id="<?php //echo $body['name'];?>">
     <div class="page">
         <table border="0" align="center" width="100%" class="statement-view text-gray-900">
@@ -171,7 +34,7 @@ body {
                 <td width="100%"  class="statement-header">
                     <table border="0" width="100%" class="statement-view text-gray-900" align="center">
                       <tr>
-                        <td align="center">รายการส่งใช้เงินยืม</td>
+                        <td align="center"><strong>รายการส่งใช้เงินยืม</strong></td>
                       </tr>
                     </table>
                     <br>
@@ -245,28 +108,7 @@ body {
                     <br>
                 </td>
             </tr>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </table>
-
-
     </div>
 
     <ul class="right-menu">
