cacf3b79ed297dcc781f19677a8d98f2b92c62a9
594407088 <594407088@parichat.skru.ac.th>
2020-07-04 11:22:44 +0700

    create file

---
+++ app_frontend/input_doc/report_debt3.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>AdminWrap - Easy to Customize Bootstrap 4 Admin Template</title>
+    <!-- Bootstrap Core CSS -->
+
+    <!-- แบมะ -->
+    <link href="../css/bootstrap_plugin.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="../css/report.css" rel="stylesheet">
+    <link href="../assets/node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- แบมะ -->
+
+    <!-- You can change the theme colors from here -->
+    <link href="../css/colors/default.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<?php
+
+            include '../../administrator/connect.php';
+
+
+
+
+
+?>
+<body id="<?php //echo $body['name'];?>">
+    <div class="page">
+
+
+<table border="1" width="100%" class="statement-view text-gray-900">
+  <tr>
+    <td width="100%">
+
+      <table border="0" width="100%" class="statement-view text-gray-900">
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td></td>
+                <td align="center">ใบสำคัญรับเงิน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td align="center">สำหรับวิทยากร</td>
+                <td></td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="left" width="20%">&nbsp;&nbsp;&nbsp;ใบสำคัญรับเงิน</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+              </tr>
+              <tr>
+                <td align="left" width="20%">&nbsp;&nbsp;&nbsp;สำหรับวิทยากร</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="left" width="50%">
+                  <table border="0" width="100%" class="statement-view text-gray-900">
+                    <tr>
+                      <td width="50%"></td>
+                      <td width="7%">วันที่</td>
+                      <td class="border-0 padding-0 text-center">
+                        18
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                      <td  width="8%">เดือน</td>
+                      <td class="border-0 padding-0 text-center">
+                        18
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                      <td  width="4%">ปี</td>
+                      <td class="border-0 padding-0 text-center">
+                        18
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td>
+
+                  <table border="0" width="100%" class="statement-view text-gray-900">
+                    <tr>
+                      <td align="left" width="10%">&nbsp;&nbsp;&nbsp;ข้าพเจ้า</td>
+                      <td class="border-0 padding-0 text-center">
+                        ssss
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                      <td align="left" width="15%">&nbsp;&nbsp;&nbsp;บ้านเลขที่</td>
+                      <td class="border-0 padding-0 text-center">
+                        ssss
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                    </tr>
+                  </table>
+
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+
+                  <table border="0" width="100%" class="statement-view text-gray-900">
+                    <tr>
+                      <td align="left" width="17%">&nbsp;&nbsp;&nbsp;ตำบล/แขวง</td>
+                      <td class="border-0 padding-0 text-center">
+                        ssss
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                      <td align="left" width="16%">&nbsp;&nbsp;&nbsp;อำเภอ/เขต</td>
+                      <td class="border-0 padding-0 text-center">
+                        ssss
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                      <td align="left" width="11%">&nbsp;&nbsp;&nbsp;จังหวัด</td>
+                      <td class="border-0 padding-0 text-center">
+                        ssss
+                          <div class="line-bottom-dashed"></div>
+                      </td>
+                    </tr>
+                  </table>
+
+                </td>
+              </tr>
+
+
+            </table>
+
+          </td>
+        </tr>
+
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="left" width="18%">&nbsp;&nbsp;&nbsp;ได้รับเงินจาก</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+                <td align="left" width="27%">&nbsp;&nbsp;&nbsp;รายละเอียดดังต่อไปนี้</td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="1" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="center">รายการ</td>
+                <td colspan="2" align="center">จำนวนเงิน</td>
+              </tr>
+              <tr>
+                <td width="60%">1</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td width="60%">1</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td width="60%">1</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td width="60%" align="right">บาท</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+            </table>
+            <br>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="left" width="16%">&nbsp;&nbsp;&nbsp;จำนวนเงิน(</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+                <td align="left" width="16%">)</td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="right" width="22%">&nbsp;&nbsp;&nbsp;ลงชื่อ</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+                <td align="left" width="36%">ผู้รับเงิน</td>
+              </tr>
+              <tr>
+                <td align="right" width="22%">&nbsp;&nbsp;&nbsp;(</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+                <td align="left" width="36%">)</td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+            <table border="0" width="100%" class="statement-view text-gray-900">
+              <tr>
+                <td align="right" width="22%">&nbsp;&nbsp;&nbsp;ลงชื่อ</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+                <td align="left" width="36%">ผู้จ่ายเงิน</td>
+              </tr>
+              <tr>
+                <td align="right" width="22%">&nbsp;&nbsp;&nbsp;(</td>
+                <td class="border-0 padding-0 text-center">
+                  ssss
+                    <div class="line-bottom-dashed"></div>
+                </td>
+                <td align="left" width="36%">)</td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+
+      </table>
+
+    </td>
+  </tr>
+</table>
+
+    </div>
+
+    <ul class="right-menu">
+        <li>
+            <a href="#" onclick="window.print();">
+                <span class="fa-stack hightlight fa-2x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-print fa-stack-1x fa-inverse"></i>
+                </span>
+                พิมพ์รายงาน
+            </a>
+        </li>
+        <li>
+            <a href="#" onclick="location.reload();">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-refresh fa-stack-1x fa-inverse"></i>
+                </span>
+                Refresh
+            </a>
+        </li>
+        <li>
+            <a href="#" onclick="window.close();">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-times fa-stack-1x fa-inverse"></i>
+                </span>
+                ปิดหน้าจอนี้
+            </a>
+        </li>
+    </ul>
+
+    <script src="<?php //echo site_common_node_modules_url('jquery/dist/jquery.min.js');?>"></script>
+</body>
+
+</html>
