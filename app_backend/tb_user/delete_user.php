59657f784a5d269e68c3ce11e85cdaf4cc6f5c2f
594407088 <594407088@parichat.skru.ac.th>
2020-07-20 13:37:23 +0700

    delete 2 table

--- app_backend/tb_user/delete_user.php
+++ app_backend/tb_user/delete_user.php
@@ -11,9 +11,20 @@
 
         $id = $_GET['id'];
 
-        $sql_person = "DELETE FROM tb_person WHERE id = '$id'";
+        // echo $id;
+        $sql = "SELECT * FROM tb_person WHERE id = '".$id."' ";
+        $query = mysqli_query($conn,$sql);
+        $result = mysqli_fetch_assoc($query);
+
+        $person_id = $result['person_id'];
+
+        $sql_person = "DELETE FROM tb_person WHERE person_id = '$person_id'";
 
         $db_query_person = mysqli_query($conn,$sql_person);
+
+        $sql_account_login = "DELETE FROM account_login WHERE person_id = '$person_id'";
+
+        $db_query_account_login = mysqli_query($conn,$sql_account_login);
 
         // echo "<script>alert('ลบข้อมูลเรียบร้อย')</script>";
         echo "<script>window.location='tb_user.php'</script>";
