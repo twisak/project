bb3b2809f1ad9467bf58d1f6b83c9ecd9fe41092
594407088 <594407088@parichat.skru.ac.th>
2020-08-09 00:31:48 +0700

    new login

--- administrator/ckpass.php
+++ administrator/ckpass.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	session_start();
     ini_set('display_errors', 1);
 	error_reporting(~0);
@@ -7,64 +7,64 @@
 
 
 <body>
-<?php 
+<?php
 	$username = $_POST['username'];
 	$password = $_POST['password'];
     $status = $_POST['status'];
-    
-	
+
+
 	if(isset($username)&&($password))
         {
 		$sql = "SELECT * FROM account_login WHERE username = '".$username."' AND password = '".$password."'";
 		$result = mysqli_query($conn,$sql);
-		$numse = mysqli_num_rows($result);	
+		$numse = mysqli_num_rows($result);
 		if($numse > 0){
-			
+
 			while($rsuser = mysqli_fetch_array($result))
 			{
-				
+
 				 $Username 	= $rsuser['username'];
 				 $Userpass 	= $rsuser['password'];
 				 $status = $rsuser['status'];
-				 
+
 			}
-			
+
 				 $_SESSION['username'] 	= $_POST['username'];
 			     $_SESSION['password'] 	= $_POST['password'];
 				 $_SESSION['status'] 	= $status;
-				 
-			
-			
-			
+
+
+
+
 				 switch($status){
-				
+
                      case 'Admin': header('location:http://localhost/project_student/app_backend/dashboard/index.php');
 							break;
-                         
+
 				     case 'Personal': header('location:http://localhost/project_student/app_frontend/dashboard/index.php');
 							break;
-				
+
 				 default : header("location:login.php?res=username หรือ password ผิดพลาด");
 				 			break;
 				                }
-				
-				
-			
+
+
+
 			}else{
-				
-				
+
+
 				header("location:login.php");
-			
-	
+
+
 				}
-		
+
 	}
     else
             {
-			
+
 		echo "<meta http-equiv='refresh' content='0;URL=login.php'>";
-		exit();	
-			
+		exit();
+
 			}
 
 
--- administrator/login.php
+++ administrator/login.php
@@ -1,74 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V14</title>
+	<title>YRU Document ระบบเอกสาร</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css"> -->
+	<link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+	<link href="css/google_fonts/fonts_prompt.css" rel="stylesheet" />
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/util1.css">
+	<link rel="stylesheet" type="text/css" href="css/main1.css">
 <!--===============================================================================================-->
 </head>
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100">
-			<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
-				<form class="login100-form validate-form flex-sb flex-w" role="form" action="ckpass.php" method="post" accept-charset ="UTF-8">
-					<span class="login100-form-title p-b-32">
-						Account Login
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+
+
+					<!-- <div class="card">
+			      <div class="card-body text-center"> -->
+							 <b><h4 class="card-title text-center"><u>ระบบจัดการเอกสาร</u></h4></b>
+			      <!-- </div>
+			    </div> -->
+
+
+
+					<img src="images/yru.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" action="ckpass.php" method="post">
+					<span class="login100-form-title">
+						YRU Document
 					</span>
 
-					<span class="txt1 p-b-11">
-						Username
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
-						<input class="input100" type="text" name="username" >
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100"></span>
-					</div>
-					
-					<span class="txt1 p-b-11">
-						Password
-					</span>
-					<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
-						<span class="btn-show-pass">
-							<i class="fa fa-eye"></i>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</span>
-						<input class="input100" type="password" name="password" >
-						<span class="focus-input100"></span>
 					</div>
-					
-					<div class="flex-sb-m w-full p-b-48">
-						<!-- <div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								Remember me
-							</label>
-						</div> -->
 
-						<div>
-							<a href="#" class="txt3">
-								Forgot Password?
-							</a>
-						</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
 					</div>
 
 					<div class="container-login100-form-btn">
@@ -77,30 +69,45 @@
 						</button>
 					</div>
 
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Forgot
+						</span>
+						<a class="txt2" href="#">
+							Username / Password?
+						</a>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
 				</form>
 			</div>
 		</div>
 	</div>
-	
 
-	<div id="dropDownSelect1"></div>
-	
+
+
+
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
 <!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
+	<script src="js/main1.js"></script>
 
 </body>
 </html>
